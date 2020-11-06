a716ec0ac9f946c46ebc08bb8d61d56f783e52d6
r4nc0r <samuel.seiz@gmail.com>
2020-11-06 18:15:05 +0100

    Update install_warp-ctc.sh

--- install_warp-ctc.sh
+++ install_warp-ctc.sh
@@ -4,7 +4,7 @@ mkdir -p ext/warp-ctc
 cd ext/warp-ctc
 # git clone https://github.com/baidu-research/warp-ctc.git .
 # Temporary fix for newer GPUs. See https://github.com/espnet/espnet/issues/733
-git clone https://github.com/jnishi/warp-ctc .
+git clone https://github.com/r4nc0r/warp-ctc .
 mkdir build
 cd build
 cmake ../
